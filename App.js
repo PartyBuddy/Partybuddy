cdde881266795710b10700704a68c778811bebe9
johntember7 <57440179+johntember7@users.noreply.github.com>
2019-11-06 13:22:00 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="Screen">
-      <div className="Top">
-      </div>
-      <div className="Bottom">
-        <div className="Button">
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
