7d88922dba49f21c530a94ad0e5b5c0018e45447
johntember7 <57440179+johntember7@users.noreply.github.com>
2019-11-06 13:12:12 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Screen">
+      <div className="Top">
+      </div>
+      <div className="Bottom">
+        <div className="Button">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
